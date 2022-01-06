c9b6cb60863d78f84b0223d6ee24aed339356a40
AlliesChen <cyphuaenng@pm.me>
2022-01-07 00:09:15 +0800

    Add Initializer

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # TODO List
 
 For The Odin Prject
+
+## Requirements
+
+- Have a *title*, *description*, *dueDate* and *priority* as todo-items
+- Have *projects* or separate lists of *todos*
+
+When a user first opens the app, there should be some sort of "default" project to which all of their todos are put.
+
+Users should be able to create new projects and choose which project their todos go into.
+
+Optional: Have *notes* and *checklist* as todo-items
+
+### UI
+
+1. view all projects
+2. view all todos in each project (probably just the title and duedate.. perhaps changing color for different priorities)
+3. expand a single todo to see/edit its details
+4. delete a todo
--- src/index.js
+++ src/index.js
@@ -1,23 +1,48 @@
 import './style.css';
+import {format} from 'date-fns'
 
-const DOMController = (identifier) => {
+const setHTML = (identifier) => {
 
     const select = document.querySelector(identifier.toString());
 
-    return {select}
+    function addClass(...classNames) {
+        classNames.forEach(className => {
+            select.classList.add(className);
+        });
+    }
+
+    return {select, addClass}
 };
 
-function project(title, description, priority, dueDate) {
+function project(title, description, priority, dueDate, projectName) {
     this.title = title;
     this.description = description;
     this.priority = priority;
     this.dueDate = dueDate;
+    this.projectName = projectName;
 };
 
-DOMController('body').select.classList.add('flex--col', 'aic', 'jc--btw');
+setHTML('body').addClass('flex--col', 'aic', 'jc--btw');
 
-DOMController('.container').select.classList.add('flex--col', 'aic', 'jc--btw');
+setHTML('#container').addClass('container', 'flex--col', 'aic', 'jc--btw');
 
-DOMController('header').select.classList.add('header');
+setHTML('header').addClass('header');
 
-DOMController('footer').select.classList.add('footer');
+setHTML('footer').addClass('footer');
+
+const Initializer = () => {
+    const storageCheck = (localStorage.length) ? true : false;
+    const defaultTodo = new project(
+        "TODO 1: What you gonna make today :))",
+        "Write it down, and maybe have some description to it. Also, you can add short notes, or checkbox to make things more clear 😁",
+        "Medium",
+        format(new Date(), 'MMM dd, yyyy'),
+        "myProject",
+    )
+    
+    if (!storageCheck) {
+        console.table(defaultTodo);
+    }
+}
+
+Initializer();
