241706bdecbbf804248f8ec3938c49b43d28605a
YPChen <cyphuaenng@pm.me>
2022-03-02 02:14:18 +0800

    Make up basic appearance

--- README.md
+++ README.md
@@ -6,14 +6,14 @@ For The Odin Prject
 
 ## Requirements
 
-- Have a *title*, *description*, *dueDate* and *priority* as todo-items
-- Have *projects* or separate lists of *todos*
+- Have a _title_, _description_, _dueDate_ and _priority_ as todo-items
+- Have _projects_ or separate lists of _todos_
 
 When a user first opens the app, there should be some sort of "default" project to which all of their todos are put.
 
 Users should be able to create new projects and choose which project their todos go into.
 
-Optional: Have *notes* and *checklist* as todo-items
+Optional: Have _notes_ and _checklist_ as todo-items
 
 ### UI
 
@@ -21,3 +21,5 @@ Optional: Have *notes* and *checklist* as todo-items
 2. view all todos in each project (probably just the title and duedate.. perhaps changing color for different priorities)
 3. expand a single todo to see/edit its details
 4. delete a todo
+
+Icon Reference: [Feather Icon](https://feathericons.com/)
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        index: './src/index.js',
-    },
-    devtool: 'inline-source-map',
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.ttf$/i,
-                type: 'asset/resource',
-            }
-        ]
-    }
-}
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  devtool: "inline-source-map",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.ttf$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
